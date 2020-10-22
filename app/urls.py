3f180adf581544be514d524b9826e5446982c738
leo <leonolan>
2020-10-23 01:33:48 +0330

    feature page for app module

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,10 @@ from . import views
 from .apps import APP_NAME
 app_name=APP_NAME
 urlpatterns=[
-    path('',views.BasicViews().home,name='home')
+    path('',views.BasicViews().home,name='home'),
+    path('examples/all_components/',views.ExampleViews().all_components,name='all_components'),
+    path('examples/sections/',views.ExampleViews().sections,name='sections'),
+    path('pages/profile/',views.ProfileViews().profile,name='profile'),
+    path('features/',views.BasicViews().features,name='features'),
 ]
 
--- app/views.py
+++ app/views.py
@@ -27,6 +27,22 @@ class BasicViews(View):
     def home(self,request,*args,**kwargs):
         context=getContext(request)
         return render(request,TEMPLATE_ROOT+'index.html',context)
+    def features(self,request,*args,**kwargs):
+        context=getContext(request)
+        return render(request,TEMPLATE_ROOT+'features.html',context)
+
+class ProfileViews(View):
+    def profile(self,request,*args,**kwargs):
+        context=getContext(request)
+        return render(request,TEMPLATE_ROOT+'profile.html',context)
+
+class ExampleViews(View):
+    def all_components(self,request,*args,**kwargs):
+        context=getContext(request)
+        return render(request,TEMPLATE_ROOT+'examples/all_components.html',context)
+    def sections(self,request,*args,**kwargs):
+        context=getContext(request)
+        return render(request,TEMPLATE_ROOT+'examples/sections.html',context)
 
 
 
