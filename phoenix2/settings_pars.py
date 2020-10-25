0c8d4bca8ebd79cc4a55f298e53943f23a5d887d
leo <leonolan>
2020-10-26 02:53:06 +0330

    run on Pars

--- phoenix2/settings_pars.py
+++ phoenix2/settings_pars.py
@@ -22,5 +22,6 @@ MEDIA_ROOT='/home2/khafonli/public_html/media/'
 STATIC_URL = SITE_URL+'static/'
 MEDIA_URL =  SITE_URL+'media/'
 PUSHER_IS_ENABLE=True
-DEBUG=True
+DEBUG=False
+
 # source /home2/khafonli/virtualenv/phoenix2/3.7/bin/activate && cd /home2/khafonli/phoenix2 &&git pull && python manage.py migrate && python manage.py collectstatic --no-input
