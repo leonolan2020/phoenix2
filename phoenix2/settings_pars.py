8b85cf1923e5272dbf044c584eb616358c8f939c
leo <leonolan>
2020-10-25 06:05:25 +0330

    pusher on server pars enabled

--- phoenix2/settings_pars.py
+++ phoenix2/settings_pars.py
@@ -18,6 +18,6 @@ STATIC_ROOT='/home2/khafonli/public_html/staticfiles/'
 MEDIA_ROOT='/home2/khafonli/public_html/media/'
 STATIC_URL = SITE_URL+'static/'
 MEDIA_URL =  SITE_URL+'media/'
-PUSHER_IS_ENABLE=False
+PUSHER_IS_ENABLE=True
 DEBUG=False
 # source /home2/khafonli/virtualenv/phoenix2/3.7/bin/activate && cd /home2/khafonli/phoenix2 &&git pull && python manage.py migrate && python manage.py collectstatic --no-input
