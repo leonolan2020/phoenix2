d438b9a27084a424f2ea289cdd17a9397c9cf04c
leo <leonolan>
2020-10-30 03:11:50 +0330

    debug on server

--- phoenix2/settings_pars.py
+++ phoenix2/settings_pars.py
@@ -22,6 +22,6 @@ MEDIA_ROOT='/home2/khafonli/public_html/phoenix/media/'
 STATIC_URL = SITE_URL+'static/'
 MEDIA_URL =  SITE_URL+'media/'
 PUSHER_IS_ENABLE=True
-DEBUG=False
+DEBUG=True
 
 # source /home2/khafonli/virtualenv/phoenix2/3.7/bin/activate && cd /home2/khafonli/phoenix2 &&git pull && python manage.py migrate && python manage.py collectstatic --no-input
